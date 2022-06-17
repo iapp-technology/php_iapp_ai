2e155f1ead8cd647dbf065eadf37bd33861db3cd
Rose <sreyroth.phe@gmail.com>
2022-06-17 09:12:07 +0700

    add extra api

--- iapp_ai/module_api.php
+++ iapp_ai/module_api.php
@@ -14,14 +14,11 @@
 class api{
 
   //  var $apikey;
-   public function apikey($apikey){
-			
+  public function apikey($apikey){
     $GLOBALS['apikey'] = $apikey;
-      // return $apikey
-   }
+  }
 
-   public function idcard_front($filepath){
-
+  public function idcard_front($filepath){
       $request = new HTTP_Request2();
       $request->setUrl('https://api.iapp.co.th/thai-national-id-card/v3/front');
       $request->setMethod(HTTP_Request2::METHOD_POST);
@@ -45,8 +42,9 @@ class api{
        echo 'Error: ' . $e->getMessage();
       }
    }
+
    public function idcard_back($filepath){
-   
+    $request = new HTTP_Request2();
    $request->setUrl('https://api.iapp.co.th/thai-national-id-card/v3/back');
    $request->setMethod(HTTP_Request2::METHOD_POST);
    $request->setConfig(array(
@@ -71,10 +69,387 @@ class api{
    }
   }
 
-  //  public function generate() {
-  //     return $this->idcard_front();
-  //   }
+  public function license_plate($filepath){
+    $request = new HTTP_Request2();
+    $request->setUrl('https://api.iapp.co.th/license-plate-recognition/file');
+    $request->setMethod(HTTP_Request2::METHOD_POST);
+    $request->setConfig(array(
+     'follow_redirects' => TRUE
+    ));
+    $request->setHeader(array(
+     'apikey' => $GLOBALS['apikey']
+    ));
+    $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+    try {
+     $response = $request->send();
+     if ($response->getStatus() == 200) {
+       echo $response->getBody();
+     }
+     else {
+       echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+       $response->getReasonPhrase();
+     }
+    }
+    catch(HTTP_Request2_Exception $e) {
+     echo 'Error: ' . $e->getMessage();
+    }
+   }
+
+   public function bookbank_ocr($filepath){
+    $request = new HTTP_Request2();
+    $request->setUrl('https://api.iapp.co.th/book-bank-ocr/file');
+    $request->setMethod(HTTP_Request2::METHOD_POST);
+    $request->setConfig(array(
+     'follow_redirects' => TRUE
+    ));
+    $request->setHeader(array(
+     'apikey' => $GLOBALS['apikey']
+    ));
+    $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+    try {
+     $response = $request->send();
+     if ($response->getStatus() == 200) {
+       echo $response->getBody();
+     }
+     else {
+       echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+       $response->getReasonPhrase();
+     }
+    }
+    catch(HTTP_Request2_Exception $e) {
+     echo 'Error: ' . $e->getMessage();
+    }
+   }
    
+   public function faceliveness_detection($filepath){
+    $request = new HTTP_Request2();
+    $request->setUrl('https://api.iapp.co.th/passive-face-liveness-detection');
+    $request->setMethod(HTTP_Request2::METHOD_POST);
+    $request->setConfig(array(
+     'follow_redirects' => TRUE
+    ));
+    $request->setHeader(array(
+     'apikey' => $GLOBALS['apikey']
+    ));
+    $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+    try {
+     $response = $request->send();
+     if ($response->getStatus() == 200) {
+       return $response->getBody();
+     }
+     else {
+       echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+       $response->getReasonPhrase();
+     }
+    }
+    catch(HTTP_Request2_Exception $e) {
+     echo 'Error: ' . $e->getMessage();
+    }
+   }
+
+   public function info_faceliveness($taskGuid){
+    $request = new HTTP_Request2();
+    $url = 'https://api.iapp.co.th/passive-face-liveness-detection/'.trim($taskGuid,'"');
+    $request->setUrl($url);
+    $request->setMethod(HTTP_Request2::METHOD_GET);
+    $request->setConfig(array(
+     'follow_redirects' => TRUE
+    ));
+    $request->setHeader(array(
+     'apikey' => $GLOBALS['apikey']
+    ));
+    try {
+     $response = $request->send();
+     if ($response->getStatus() == 200) {
+       echo $response->getBody();
+     }
+     else {
+       echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+       $response->getReasonPhrase();
+     }
+    }
+    catch(HTTP_Request2_Exception $e) {
+     echo 'Error: ' . $e->getMessage();
+    }
+   }
+
+  public function signature_detection($filepath){
+    $request = new HTTP_Request2();
+    $request->setUrl('https://api.iapp.co.th/book-bank-ocr/file');
+    $request->setMethod(HTTP_Request2::METHOD_POST);
+    $request->setConfig(array(
+      'follow_redirects' => TRUE
+    ));
+    $request->setHeader(array(
+      'apikey' => $GLOBALS['apikey']
+    ));
+    $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+    try {
+      $response = $request->send();
+      if ($response->getStatus() == 200) {
+        echo $response->getBody();
+      }
+      else {
+        echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+        $response->getReasonPhrase();
+      }
+    }
+    catch(HTTP_Request2_Exception $e) {
+      echo 'Error: ' . $e->getMessage(); 
+    }
+  }
+
+public function water_file_ocr($filepath){
+$request = new HTTP_Request2();
+$request->setUrl('https://api.iapp.co.th/meter-number-ocr/file');
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'apikey' => $GLOBALS['apikey']
+));
+$request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
 }
 
+
+public function water_base64_ocr($filepath){
+$request = new HTTP_Request2();
+$request->setUrl('https://api.iapp.co.th/meter-number-ocr/base64');
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'apikey' =>  $GLOBALS['apikey']
+));
+$base64 = fopen($filepath,"r");
+fclose($base64);
+$request->setBody($base64);
+
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
+}
+
+public function asr($filepath){
+$request = new HTTP_Request2();
+$request->setUrl('https://api.iapp.co.th/asr');
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'apikey' =>  $GLOBALS['apikey']
+));
+$request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
+}
+
+public function kaitom_tts($text){
+$request = new HTTP_Request2();
+$request->setUrl('https://api.iapp.co.th/thai-tts-kaitom/tts?text=' .$text);
+$request->setMethod(HTTP_Request2::METHOD_GET);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'apikey' => $GLOBALS['apikey']
+));
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
+}
+
+public function cee_tts($text){
+  $request = new HTTP_Request2();
+  $request->setUrl('https://api.iapp.co.th/thai-tts-cee/tts?text=' .$text);
+  $request->setMethod(HTTP_Request2::METHOD_GET);
+  $request->setConfig(array(
+    'follow_redirects' => TRUE
+  ));
+  $request->setHeader(array(
+    'apikey' => $GLOBALS['apikey']
+  ));
+  try {
+    $response = $request->send();
+    if ($response->getStatus() == 200) {
+      echo $response->getBody();
+    }
+    else {
+      echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+      $response->getReasonPhrase();
+    }
+  }
+  catch(HTTP_Request2_Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
+  }
+public function img_bg_removal($filepath){}
+
+public function thai_qa($question, $document){}
+
+public function thai_qgen($text){}
+
+public function face_verification($filepath){}
+
+public function face_detect_single($filepath, $company_name ){
+  $request = new HTTP_Request2();
+  $request->setUrl('https://api.iapp.co.th/face_detect_single');
+  $request->setMethod(HTTP_Request2::METHOD_POST);
+  $request->setConfig(array(
+    'follow_redirects' => TRUE
+  ));
+  $request->setHeader(array(
+    'apikey' => $GLOBALS['apikey']
+  ));
+
+  // Use score of each company
+  $request->addPostParameter(array(
+    'company' => $company_name 
+  ));
+
+  // Use default score
+  //$request->addPostParameter(array(
+  //  'company' => '{Your Company Name}' 
+  //));
+
+  $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+  try {
+    $response = $request->send();
+    if ($response->getStatus() == 200) {
+      echo $response->getBody();
+    }
+    else {
+      echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+      $response->getReasonPhrase();
+    }
+  }
+  catch(HTTP_Request2_Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
+}
+
+public function face_detect_multi($filepath, $company_name){
+  $request = new HTTP_Request2();
+  $request->setUrl('https://api.iapp.co.th/face_detect_multi');
+  $request->setMethod(HTTP_Request2::METHOD_POST);
+  $request->setConfig(array(
+    'follow_redirects' => TRUE
+  ));
+  $request->setHeader(array(
+    'apikey' =>  $GLOBALS['apikey']
+  ));
+
+  // Use score of each company
+  $request->addPostParameter(array(
+    'company' => $company_name 
+  ));
+
+  // Use default score
+  //$request->addPostParameter(array(
+  //  'company' => '{Your Company Name}'
+  //));
+
+  $request->addUpload('file', $filepath, $filepath, '<Content-Type Header>');
+  try {
+    $response = $request->send();
+    if ($response->getStatus() == 200) {
+      echo $response->getBody();
+    }
+    else {
+      echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+      $response->getReasonPhrase();
+    }
+  }
+  catch(HTTP_Request2_Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
+}
+
+public function face_detection_tool($detect_val,$company_name, $password ){
+$request = new HTTP_Request2();
+
+// Configure Score
+$url1 = 'https://api.iapp.co.th/face_config_score?detection='.$detect_val.'&company='.$company_name.'&password='.$password;
+$request->setUrl($url1);
+
+// Show Score
+$url2 = 'https://api.iapp.co.th/face_config_score?detection&company='.$company_name.'&password='.$password;
+$request->setUrl($url2);
+
+$request->setMethod(HTTP_Request2::METHOD_GET);
+$request->setConfig(array(
+  'follow_redirects' => TRUE
+));
+$request->setHeader(array(
+  'apikey' => $GLOBALS['apikey']
+));
+try {
+  $response = $request->send();
+  if ($response->getStatus() == 200) {
+    echo $response->getBody();
+  }
+  else {
+    echo 'Unexpected HTTP status: ' . $response->getStatus() . ' ' .
+    $response->getReasonPhrase();
+  }
+}
+catch(HTTP_Request2_Exception $e) {
+  echo 'Error: ' . $e->getMessage();
+}
+}
+
+public function face_recognition($filepath){}
+
+
+
+
+
+}
 ?>
--- test.php
+++ test.php
@@ -3,9 +3,28 @@ ini_set("include_path", "iapp_ai");
 require_once 'iapp_ai\module_api.php';
 
 $object = new api;
-$object->apikey("I9waueVqWl9bxsZ3CfTPiAth61kOnSSV");
-$object->idcard_front("media/id-card-front.jpg");
-$object->idcard_back("media/id-card-back.jpg");
+$object->apikey("ClmXkT5g4kAWkTIyzRbBViyjNCHM33oH");
+// $object->idcard_front("media/id-card-front.jpg");
+// $object->idcard_back("media/id-card-back.jpg");
+// $object->license_plate("media/plate.jpg");
+// $object->bookbank_ocr("media/E7393203-15.jpg");
+// $object->signature_detection("media/signature.jpg");
+// $object->water_file_ocr("media/water-meter.jpg");
+// $object->water_base64_ocr("media/water-meter.txt");
 
+// $object->asr("media/data.wav");
+// $object->kaitom_tts("แค็วม์เป็นแค่หมู่บ้านใช่หรือไม่");
+// $object->cee_tts("แค็วม์เป็นแค่หมู่บ้านใช่หรือไม่");
+// $object->img_bg_removal("media/face.jpg");
+// $object->thai_qa("แค็วม์เป็นแค่หมู่บ้านใช่หรือไม่", "จังหวัดแค็วม์โปแลนด์ wojewdztwochemskie คือหน่วยการปกครองท้องถิ่นของประเทศโปแลนด์ในช่วงปี ค.ศ.1975 - ค.ศ.1998 จังหวัดได้รับการรวมเข้ากับจังหวัดลูบลินมีเมืองหลักคือแค็วม์ใน ปี ค.ศ.1998 มีพื้นที่ประมาณ 3865 ตารางกิโลเมตรและมีประชากร 248800 คน");
+// $object->thai_qgen("ผมพูดภาษาไทย");
 
-?>
+// $taskGuid=$object->faceliveness_detection("media/face.jpg");
+// $object->info_faceliveness($taskGuid);
+// $object->face_verification("media/face.jpg");
+// $object->face_detect_single("media/face.jpg","iApp");
+// $object->face_detect_multi("media/multi_face.jpg","iApp");
+// $object->face_detection_tool("0.5","iApp", "iapp");
+// $object->face_recognition("media/face.jpg");
+
+?> 
